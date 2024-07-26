eaa60c85ce2d41891c2bc3393521a31ddb7a725d
Yeswanth <yeswanthgontina@gmil.com>
2024-07-26 16:10:26 +0530

    Files successfully uploads to S3 bucket. Show the upload progress of the file.

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 function clouduploader() {
-	local file = "$1"
-	local bucket = "clouduploader12"
-	local key = "
+	local file="$1"
+	local bucket="$2"
+	local retry_attempt=3
+	if [ ! -f "$file" ]; then
+		echo "Error!! File $file not found!"
+		exit 1
+	fi
+	for((i=0; i<retry_attempt; i++)); do
+		if pv -pterb "$file" | aws s3 cp "$file" "s3://$bucket/" 2>/dev/null; then
+			echo "File Uploaded Successfully"
+			return 0
+		fi
+		echo "Upload Failed! Retrying in 3 seconds. Please wait..."
+		sleep 3
+	done
+	echo "Uploading Failed after $retry_attempt times"
+	exit 1
 }
+
+clouduploader "$1" "$2"
